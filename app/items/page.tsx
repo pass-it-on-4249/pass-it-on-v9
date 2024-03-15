bb2566f74c0a3d53d3dfb12d5fddfa9dcaab3f03
Ashraf Jaffar <ashrafjfr@Ashrafs-MacBook-Pro.local>
2024-03-15 19:01:34 +0800

    done my cart

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -7,6 +7,7 @@ import SearchBar from "@/components/searchBar";
 import Category from "@/components/category";
 import SearchButton from "@/components/searchButton";
 import ResetButton from "@/components/resetButton";
+import PageNav from "@/components/pageNav";
 
 export default function ItemList() {
   return (
@@ -107,14 +108,17 @@ export default function ItemList() {
         <SearchButton />
         <ResetButton />
       </div>
-      <div className="flex flex-row justify-center">
-        <div className="flex flex-row "> 
-          <div className="">My Cart</div>
+      <div className="flex flex-row justify-center items-center mt-7">
+        <div className="flex flex-row items-center pr-20"> 
+          <div className="text-[#2BA41D] font-semibold pr-2">My Cart</div>
           <div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#2BA41D" className="w-5 h-5 items-center">
               <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
             </svg>
           </div>
+        </div>
+        <div className="pl-20">
+          <PageNav />
         </div>  
       </div>
     </main>
