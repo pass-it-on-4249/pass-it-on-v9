40259cd74a271372f447ffeb54f78b35b67ea831
liviamil <liviamil@u.nus.edu>
2024-03-19 22:55:21 +0800

    Update product card data

--- lib/data.ts
+++ lib/data.ts
@@ -30,3 +30,54 @@ export const links = [
         hash: "#contact-us",
     },
 ] as const;
+
+export const productData = [
+    {
+      id: 130741,
+      image: "/130741.png",
+      name: 'Queen Size Bed & Mattress',
+      description: 'Queen size bed frame & mattress',
+      address: 'Alexandra, Queenstown, Redhill, Tiong Bahru',
+      deliveryCovered: true,
+    }, 
+    {
+        id: 130724,
+        image: "/130724.png",
+        name: 'Leather Chair',
+        description: 'This is a comfortable & relaxing chair',
+        address: 'Clementi, Boon Lay',
+        deliveryCovered: true,
+    }, 
+    {
+        id: 130714,
+        image: "/130714.png",
+        name: '6 Dining Table Chairs',
+        description: '6 chairs. Stable and comfortable. 3 of them have a taped patches on...',
+        address: 'Holland Village, Orchard',
+        deliveryCovered: true,
+    },
+    {
+        id: 130713,
+        image: "/130713.png",
+        name: 'Size 36 female shoes',
+        description: 'these are worn but still usable, just require some cleaning up.',
+        address: 'Boon Lay, Jurong, Tuas',
+        deliveryCovered: true,
+    },
+    {
+        id: 130720,
+        image: "/130720.png",
+        name: 'Pokemon Ga Ole Disks',
+        description: 'This is a play disk for pokemon ga ole machine.',
+        address: 'Kent Ridge, Novena, Bugis',
+        deliveryCovered: true,
+    },
+    {
+        id: 130721,
+        image: "/130721.png",
+        name: 'Assorted Toys',
+        description: 'Random toys for children',
+        address: 'Bayshore, Bedok, Chai Chee, Eastwood',
+        deliveryCovered: true,
+    }
+  ];
