0216f6a61b08b1702816ec7a83ad06676b8d2f27
liviamil <96341083+liviamil@users.noreply.github.com>
2024-04-07 17:56:00 +0800

    Update layout.tsx with analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
@@ -16,6 +17,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google Tag Manager --> */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-MK7XH31FM1"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MK7XH31FM1');
+          `}
+        </Script>
+        {/* <!-- End Google Tag Manager --> */}
+      </head>
       <body className={`${plus_jakarta_sans.className} bg-white text-stone-900 relative`}>
         {children}
       </body>
