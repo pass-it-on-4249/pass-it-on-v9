592ad7a1ad4cfa48c0c6f26c39181f96b5f77819
limdanqi <limdanqi01@gmail.com>
2024-04-07 21:55:20 +0800

    Update to Logging v9 form

--- public/logging/logging.js
+++ public/logging/logging.js
@@ -226,9 +226,9 @@ return {
 //
 /////////////////////////////////////////////////////////////////////////////
 
-// Logging v1 submission function
+// Logging v9 submission function
 // submits to the google form at this URL:
-// docs.google.com/forms/d/e/1FAIpQLSeVPQRcJ-QTRUg6iVzxVv-MSpezkJ-lfdEt4sdmDsVzCGG3lg/viewform?usp=sf_link
+// docs.google.com/forms/d/e/1FAIpQLSeFg9jRywJxs00OMLE-KeDzqgtBEu0e4_SboWQlSWW2J68bsg/viewform?usp=sf_link
 function sendNetworkLog(
   uid,
   timeMs,
@@ -238,7 +238,7 @@ function sendNetworkLog(
   info,
   state,
   log_version) {
-var formid = "e/1FAIpQLSeVPQRcJ-QTRUg6iVzxVv-MSpezkJ-lfdEt4sdmDsVzCGG3lg";
+var formid = "e/1FAIpQLSeFg9jRywJxs00OMLE-KeDzqgtBEu0e4_SboWQlSWW2J68bsg";
 var data = {
   "entry.1613142373": uid,
   "entry.1589787878": timeMs,
